9b00a2b11d7f1e405edd5def2ff296f9f40e2b61
MokshDesai <desaimoksh06@gmail.com>
2025-12-12 16:08:42 +0530

    Add Django document summarization system

---
+++ DSS_app/temp.py
@@ -0,0 +1,8 @@
+import PyPDF2
+import docx
+import pytesseract
+from pdf2image import convert_from_path
+from PIL import Image
+import pytesseract
+
+print("All extraction libraries installed successfully ")
